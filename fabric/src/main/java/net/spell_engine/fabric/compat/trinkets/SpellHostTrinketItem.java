e6914ca44883a57b8a40b887785fa757a2209b16
vao211 <vaolu211@gmail.com>
2026-06-27 07:30:34 +0700

    Fix: Item rollback, Desync

--- fabric/src/main/java/net/spell_engine/fabric/compat/trinkets/SpellHostTrinketItem.java
+++ fabric/src/main/java/net/spell_engine/fabric/compat/trinkets/SpellHostTrinketItem.java
@@ -1,11 +1,21 @@
 package net.spell_engine.fabric.compat.trinkets;
 
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.Multimap;
 import dev.emi.trinkets.api.SlotReference;
 import dev.emi.trinkets.api.TrinketItem;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.attribute.EntityAttribute;
+import net.minecraft.entity.attribute.EntityAttributeModifier;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.sound.SoundEvent;
+import net.minecraft.util.Identifier;
+
+import java.util.Locale;
+import java.util.Map;
 
 public class SpellHostTrinketItem extends TrinketItem {
     private final SoundEvent equipSound;
@@ -13,6 +23,34 @@ public class SpellHostTrinketItem extends TrinketItem {
     public SpellHostTrinketItem(Settings settings, SoundEvent equipSound) {
         super(settings);
         this.equipSound = equipSound;
+    }
+
+    @Override
+    public Multimap<RegistryEntry<EntityAttribute>, EntityAttributeModifier> getModifiers(ItemStack stack, SlotReference slot, LivingEntity entity, Identifier slotIdentifier) {
+        Multimap<RegistryEntry<EntityAttribute>, EntityAttributeModifier> uniqueModifiers = ArrayListMultimap.create();
+
+        try {
+            Multimap<RegistryEntry<EntityAttribute>, EntityAttributeModifier> defaultModifiers = super.getModifiers(stack, slot, entity, slotIdentifier);
+            String itemName = Registries.ITEM.getId(stack.getItem()).getPath();
+
+            for (Map.Entry<RegistryEntry<EntityAttribute>, EntityAttributeModifier> entry : defaultModifiers.entries()) {
+                EntityAttributeModifier modifier = entry.getValue();
+                String attributeName = entry.getKey().value().getTranslationKey().replace("attribute.name.", "");
+
+                String rawPath = slotIdentifier.getPath() + "_" + itemName + "_base_" + attributeName + "_" + modifier.operation().name();
+
+                String safePath = rawPath.toLowerCase(Locale.ROOT).replaceAll("[^a-z0-9/._-]", "_");
+
+                uniqueModifiers.put(entry.getKey(), new EntityAttributeModifier(
+                        Identifier.of(slotIdentifier.getNamespace(), safePath),
+                        modifier.value(),
+                        modifier.operation()
+                ));
+            }
+        } catch (Exception ignored) {
+        }
+
+        return uniqueModifiers;
     }
 
     @Override
